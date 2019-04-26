8cd8d9c4ab87805e62cf6f6d84a793899bf97ca7
Andrew Wilk <andrewwilk@modv-ve504-0694.apn.wlan.upenn.edu>
2019-04-26 12:27:29 -0400

    reading csv

--- import.py
+++ import.py
@@ -1,23 +1,33 @@
 import psycopg2 as psy
-import pandas as pd 
+import csv
 import sys
 
 def load_table(file_path, table_name, db, host, port, user, pwd):
     try:
         print('start')
-        print(host)
+
         conn = psy.connect(dbname=db, host=host, port=port,\
         user=user, password=pwd)
-        print('connecting')
+        print('connected')
+        
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM cohorts')
+        # cursor.execute('SELECT * FROM cohorts')
+        # open csv, for each file make insert and cursor.execute(//insert//) 
+        with open(file_path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row['note'])       
+
+
+
+
         conn.close()
     except Exception as e:
         print(str(e))
         sys.exit(1)
 
-file_path = 'wow.txt'
-table_name = 'consent_sandbox'
+file_path = './update_test.csv'
+table_name = 'cohorts'
 db = 'consent_sandbox'
 host = '158.130.176.63'
 user = 'postgres'
@@ -25,3 +35,5 @@ port = 5432
 pwd = 'root'
 
 load_table(file_path, table_name, db, host, port, user, pwd)
+
+
